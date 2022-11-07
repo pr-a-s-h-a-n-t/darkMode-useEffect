600ca0b5513c4277e18618ff6ac82aef14578294
Prashant Mishra <prashantmishra111@outlook.com>
2022-11-07 09:53:50 +0530

    done

--- darkmode/src/components/Main.js
+++ darkmode/src/components/Main.js
@@ -3,13 +3,13 @@ import React from "react"
 export default function Main(props) {
     return (
         <main className={props.darkMode ? "dark" : ""}>
-            <h1 className="main--title">Fun facts about React</h1>
+            <h1 className="main--title">Hello Developer...</h1>
             <ul className="main--facts">
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
+                <li>React Was first released in 2013</li>
+                <li>React Was originally created by Jordan Walke</li>
+                <li>React Has well over 100K stars on GitHub</li>
+                <li>React Is maintained by Facebook</li>
+                <li>React Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
         </main>
     )
